3040194a60f8b07fe01a7f7e0d56892baf6a2481
Raghav Agrawal <raghav.github69@gmail.com>
2025-05-28 17:15:19 +0530

    corsfix

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -22,7 +22,7 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.WithOrigins("https://raghavvag.github.io") // Allow your GitHub Pages origin
+        policy.AllowAnyOrigin() // Allow all origins for now
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
